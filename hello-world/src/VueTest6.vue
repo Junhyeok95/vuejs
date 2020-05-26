9a2d350aa83ca1201354bb6137d9fcc1fde555c5
JANG JUNHYEOK <btrya23@gmail.com>
2020-05-26 22:37:23 +0900

    Vue Study 05, Vuex ...?

---
+++ hello-world/src/VueTest6.vue
@@ -0,0 +1,28 @@
+// Flux 패턴
+// Action -> Dispatcher ->Store -> View
+
+// Vuex
+// 상태 ... 게터 ... 변이 ... 액션 ... 
+
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+import store from 'src/store';
+
+export default {
+  computed: {
+    count () {
+      return store.state.count;
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ hello-world/src/VueTest7.vue
@@ -0,0 +1,24 @@
+// Vuex import 사용 후 
+
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+import store from 'src/store';
+
+export default {
+  computed: {
+    count () {
+      return this.$store.state.count;
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
