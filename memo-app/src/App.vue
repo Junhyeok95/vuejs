cf84764c7b30e0dd6688c57bab11f4403f4bd221
JANG JUNHYEOK <btrya23@gmail.com>
2020-05-29 22:27:29 +0900

    Vue Study 08, MemoApp init

---
+++ memo-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <app-header/>
+    
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader';
+
+export default {
+  name: 'app',
+  components: {
+    AppHeader
+  }
+  // data () {
+  //   return {
+  //     msg: 'Welcome to Your Vue.js App'
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
